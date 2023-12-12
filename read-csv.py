e85269f802ca7e13a247d20fa5ebd5a2185476bc
tarneo <tarneo@tarneo.fr>
2023-12-12 13:25:29 +0100

    Make a good graph in read-csv.py

--- read-csv.py
+++ read-csv.py
@@ -7,8 +7,53 @@ with open('positions.csv', 'r') as file:
     #     id, x, y = line.split(',')
     data = np.loadtxt(file, delimiter=';', skiprows=0)
 
+data = data[:, 0:2]
+
+# Offset timing to start at 0
+data[:, 0] -= data[0, 0]
+
+# Make the time be in seconds
+data[:, 0] /= 1000
+
+# Triggers every 5 meters
+triggers = [i for i in range(500, 50, -50)]
+
+passes = []
+
+def pos_to_trigger(pos):
+    for i, trigger in enumerate(triggers):
+        if pos > trigger:
+            return i
+    return -1
+
+prev_trigger = pos_to_trigger(data[0, 1])
+
+for data_point in data[1:]:
+    trigger = pos_to_trigger(data_point[1])
+    if trigger != prev_trigger:
+        passes.append([data_point[0], trigger])
+        prev_trigger = trigger
+
+passes = np.array(passes)
+
+# Make the trigger be in meters
+passes[:, 1] = passes[:, 1] * 5
+
 # Make two plots for x and y
-plt.plot(data[:, 0], data[:, 1], label='x', marker='.', linestyle=' ')
-# plt.plot(data[:, 0], data[:, 2], label='y', marker='.', linestyle=' ')
-plt.legend()
+plt.plot(passes[:, 0], passes[:, 1], label='pos', marker='.', linestyle='-')
+
+speeds = [
+    5 / (passes[i+1, 0] - passes[i, 0])
+    for i in range(len(passes) - 1)
+]
+
+speed_times = passes[1:, 0] - (passes[1:, 0] - passes[:-1, 0]) / 2
+
+print(passes, speeds)
+
+plt.plot(speed_times, speeds, label='speed', marker='.', linestyle='-')
+
+# Add legend
+plt.legend(loc='upper left')
+
 plt.show()
